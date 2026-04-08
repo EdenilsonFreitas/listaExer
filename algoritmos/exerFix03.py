c47c93d6f66d9d4b444144ba3b25fa287e455832
Edenilson Freitas <freitaseddie126@gmail.com>
2026-04-08 00:35:31 +0000

    praticando

---
+++ algoritmos/exerFix03.py
@@ -0,0 +1,29 @@
+# Questao01
+"""
+Escreva um programa que leia dois valores que representem o início
+e o fim de um intervalo. O programa deverá ler um terceiro valor
+digitado e verificar se este terceiro valor está dentro do intervalo ou
+fora. Caso esteja fora do intervalo, deverá informar se está na parte
+inferior ou superior do intervalo.
+"""
+# num1 = int(input("Digite o início do intervalo: "))
+# num2 = int(input("Digite o fim do intervalo: "))
+# num3 = int(input("Digite o terceiro valor: "))
+
+# if num3 < num1:
+#     print("O terceiro valor está abaixo do intervalo.")
+# elif num3 > num2:
+#     print("O terceiro valor está acima do intervalo.")
+# else:
+#     print("O terceiro valor está dentro do intervalo.")
+
+# Questao 02
+
+"""
+Escreva um programa que sempre escolhe o menor caminho a ser percorrido pelo
+usuário em função do local onde ele está e as opções de caminho a serem seguidas. O
+usuário sempre parte do ponto A (Início) em direção ao Fim (D, E, F ou G). O usuário
+deverá fornecer as distância entre os pontos e o programa deverá apresentar o caminho
+a ser percorrido e a distância percorrida. Utilizar apenas estruturas condicionais.
+"""
+
